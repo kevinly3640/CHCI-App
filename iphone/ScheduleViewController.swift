e66d6fe5e18ee791efe90eb29dffe092f70fbfd1
CS Student <Student@zpr-l0007077.local>
2017-12-19 10:16:00 -0500

    merged

--- iphone/ScheduleViewController.swift
+++ iphone/ScheduleViewController.swift
@@ -13,7 +13,6 @@ class ScheduleViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-     
 
     }
 
